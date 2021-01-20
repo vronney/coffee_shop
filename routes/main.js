9b704dcc78223b49c668263f40dede6b2f30c186
vronney <ronneyvargas@gmail.com>
2021-01-20 14:39:00 -0600

    Created partials folder to hold footer, head, nav, scripts files

--- routes/main.js
+++ routes/main.js
@@ -2,7 +2,12 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
-    res.render('home')
+    const data = {
+        title: 'Manchitas By Elsa',
+        description: 'Welcome to our house of comida! We hope you enjoy our broad selection of comidas from Mexico.'
+    }
+
+    res.render('home', data)
 })
 
 module.exports = router
